b5dd7c22dcb17ec031144f724da78f44982ea97e
ac <achotai2@gmail.com>
2020-10-20 14:29:20 -0700

    Got working, now will make changes to bring in the temporal network approach.

--- pongplay.py
+++ pongplay.py
@@ -152,6 +152,7 @@ class Agent:
         self.ballEncoderY    = ScalarEncoder( self.ballYEncodeParams )
         self.ballEncoderVelX = RDSE( self.ballVelEncodeParams )
         self.ballEncoderVelY = RDSE( self.ballVelEncodeParams )
+        self.motorEncoder    = ScalarEncoder ( self.motorEncodeParams )
 
         self.encodingWidth = ( self.paddleEncoder.size + self.ballEncoderX.size + self.ballEncoderY.size +
             self.ballEncoderVelX.size + self.ballEncoderVelY.size )
@@ -175,10 +176,10 @@ class Agent:
 
         # Set up Motor Network
         self.mn = SpatialPooler(
-            inputDimensions            = (self.encodingWidth,),
+            inputDimensions            = (3699,),
             columnDimensions           = (100,),
             potentialPct               = 0.85,
-            potentialRadius            = self.encodingWidth,
+            potentialRadius            = 3699,
             globalInhibition           = True,
             localAreaDensity           = 0,
             numActiveColumnsPerInhArea = 10,
@@ -190,29 +191,39 @@ class Agent:
             wrapAround                 = True
         )
 
-    def EncodeData( self, yPos, ballX, ballY, ballXSpeed, ballYSpeed ):
+        tm = TemporalMemory(
+            columnDimensions          = (3699,),
+            cellsPerColumn            = 32,
+            activationThreshold       = 16,
+            initialPermanence         = 0.21,
+            connectedPermanence       = 0.1,
+            minThreshold              = 12,
+            maxNewSynapseCount        = 20,
+            permanenceIncrement       = 0.1,
+            permanenceDecrement       = 0.1,
+            predictedSegmentDecrement = 0.0,
+            maxSegmentsPerCell        = 128,
+            maxSynapsesPerSegment     = 32,
+            seed=42
+        )
+
+    def EncodeData( self, yPos, ballX, ballY, ballXSpeed, ballYSpeed, motorInput ):
         # Now we call the encoders to create bit representations for each value, and encode them.
         paddleBits  = self.paddleEncoder.encode( yPos )
         ballBitsX    = self.ballEncoderX.encode( ballX )
         ballBitsY    = self.ballEncoderY.encode( ballY )
         ballBitsVelX = self.ballEncoderVelX.encode( ballXSpeed )
         ballBitsVelY = self.ballEncoderVelY.encode( ballYSpeed )
+        if motorInput != 0:
+            motorBits = self.motorEncoder.encode( motorInput )
 
         # Concatenate all these encodings into one large encoding for Spatial Pooling.
-        encoding = SDR( self.encodingWidth ).concatenate( [ paddleBits, ballBitsX, ballBitsY, ballBitsVelX, ballBitsVelY ] )
+        if motorInput == 0:
+            encoding = SDR( self.encodingWidth ).concatenate( [ paddleBits, ballBitsX, ballBitsY, ballBitsVelX, ballBitsVelY ] )
+        else:
+            encoding = SDR( self.encodingWidth + self.motorEncoder.size ).concatenate( [ paddleBits, ballBitsX, ballBitsY, ballBitsVelX, ballBitsVelY, motorBits ] )
+
         return encoding
-
-    def GetSDR( self, encoding, ID, learning ):
-        # Create an SDR to represent active columns, This will be populated by the
-        # compute method below. It must have the same dimensions as the Spatial Pooler.
-        if ID == "sp":
-            activeColumns = SDR( self.sp.getColumnDimensions() )
-            self.sp.compute(encoding, learning, activeColumns)
-        elif ID == "mn":
-            activeColumns = SDR( self.mn.getColumnDimensions() )
-            self.mn.compute(encoding, learning, activeColumns)
-
-        return activeColumns
 
     def FeelingTrig( self, feeling ):
         # Triggered when agent is fed a feeling state.
@@ -225,10 +236,12 @@ class Agent:
                 for motCell in activeMotorCells.sparse:       # Go through all the active motor cells in this buffer time step...
                     permanence = numpy.zeros(3699, dtype=numpy.float32)
                     self.mn.getPermanence( motCell, permanence, 0.0 )   # and get their strength of connections to the sp cells.
-#                    print ( permanence )
                     for actCell in activeCells.sparse:         # Get all the active sp cells in this time step...
                             # and adjust their connection strength to this active motor cell accordingly.
-                            permanence[actCell] += ( feeling * 0.05 ) * ( self.bufferSize - ind )
+                            # (connection strength should vary between -1 and 1, so we use a tanh function)
+                            permVal = numpy.tan( permanence[ actCell ] )
+                            permVal += ( feeling * 0.01 ) * ( self.bufferSize - ind )
+                            permanence[ actCell ] = numpy.tanh( permVal )
 
                     self.mn.setPermanence( motCell, permanence )
 
@@ -236,13 +249,14 @@ class Agent:
         # Agents brain center.
 
         # Run SP with current sense data without motor input, with learning:
-        encoding = self.EncodeData( yPos, ballX, ballY, ballXSpeed, ballYSpeed )
-        activeCells = self.GetSDR( encoding, "sp", True )
+        encoding = self.EncodeData( yPos, ballX, ballY, ballXSpeed, ballYSpeed, 0 )
+        activeCells = SDR( self.sp.getColumnDimensions() )
+        self.sp.compute(encoding, True, activeCells)
 
         # Choose motor output:
         # Feed SP activated cells to motor network.
-        encoding = self.EncodeData( yPos, ballX, ballY, ballXSpeed, ballYSpeed )
-        activeMotorCells = self.GetSDR( encoding, "mn", True )
+        activeMotorCells = SDR( self.mn.getColumnDimensions() )
+        self.mn.compute(activeCells, True, activeMotorCells)
 
         # Choose winning motor action by adding up points for activated cells.
         motorScore = [ 0, 0, 0 ]         # Keeps track of each motor output weighted score UP, STILL, DOWN
@@ -316,7 +330,7 @@ while True:
         ball.goto(0, 0)
         ball.dx *= -1
         ball.dy *= random.choice([-1, 1])
-        leftAgent.FeelingTrig( numpy.exp( -numpy.absolute( ball.ycor() - paddle_a.ycor() ) / 200 ) - 1 )
+        leftAgent.FeelingTrig( -1 )
 
     # Paddle and ball collisions
     if ball.xcor() < -340 and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
